438f051016330ee89fbddf6f6489e380470e6958
Mariana <marianagutierrezvivi@gmail.com>
2024-11-18 20:46:19 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,33 +4,33 @@ const chalk = require('chalk')
 //contact details
 global.ownernumber = ['254732647560']
 global.ownername = "CULTURE"//owner name
-global.ytname = "YT: Kish-MD"
-global.socialm = "GitHub: Kish-MD"
+global.ytname = "YT: Sparky-MD"
+global.socialm = "GitHub: Sparky-MD"
 global.location = "Kenya"
 
-global.botname = 'KISH-MD' //name of the bot
+global.botname = 'Sparky-MD' //name of the bot
 
 //sticker details
-global.stickername = 'KISH-MD'
+global.stickername = 'Sparky-MD'
 global.packname = 'Sticker By'
-global.author = 'Kish Bot'
+global.author = 'Sparky Bot'
 //console view/theme
 global.themeemoji = '𓅃'
-global.wm = "Kish botz inc."
+global.wm = "Mariana2024"
 
 //theme link
-//global.link = 'https://chat.whatsapp.com/LhBwWwQAS4y93XOsCKpxdv'
+//global.link = ''
 
 //custom prefix
-global.prefa = ['.']
+global.prefa = ['-']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
-global.autoRecording = true //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = false //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
 global.autoread_status = true //auto view status/story
 
@@ -38,7 +38,7 @@ global.autoread_status = true //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 Kish MD 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/Brashokish/Kish-MD\n',
+    done: '*here you go!* \n\n*🎀Sparky MD🎀*\n\n*🧩 Bot link:* \nhttps://github.com/Marianavivi/Sparky-MD\n',
     prem: '*This feature can be used by premium user only*',
     admin: '*This feature can be used by admin only*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
